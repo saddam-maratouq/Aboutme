f6f808db17536f5867f9f4558b4666ca6433da2d
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-06-21 21:13:51 +0300

    new page

---
+++ app.js
@@ -0,0 +1,130 @@
+" use strict";
+
+alert(" welcome MY  heart  ");
+
+let userName = prompt(" whats your name  ");
+
+alert(" nice to meet you  " + userName);
+
+alert("welcome Dear " + userName);
+
+
+
+
+
+let q1 = prompt("do you think Iam like vieo game ? yes/y no/n  ").toLowerCase();
+
+switch (q1) {
+  case "yes":
+
+  case "y":
+    // console.log("great");
+    alert(" great Dear ");
+
+    break;
+
+  case "no":
+  case " n ":
+    // console.log("its not right");
+    alert(" its not right");
+
+    break;
+
+  default:
+    // console.log(" you must type yes or no ");
+    alert(" you must type yes or no ");
+}
+
+
+
+let q2 = prompt("do you think I like swim ? yes/y no/n ").toLowerCase();
+
+switch (q2) {
+  case "yes":
+  case "y":
+    // console.log("wrong");
+    alert(" I didnt learn well ");
+
+    break;
+
+  case "no":
+  case "n":
+    // console.log("well done");
+    alert("well done you gess that ");
+
+    break;
+
+  default:
+    // console.log(" you must type yes/y no/n ");
+    alert(" you must type yes or no ");
+}
+
+
+
+
+let q3 = prompt("do you think I like spagtie ? yes/y no/n  ").toLowerCase();
+
+switch (q3) {
+  case "yes":
+  case "y":
+    // console.log("great");
+    alert(" y love it too  ");
+
+    break;
+
+  case "no":
+  case "n":
+    // console.log("wrong dear");
+    alert(" you have to try taste it  ");
+
+  default:
+    // console.log(" you must type yes or no ");
+    alert(" you must type yes or no ");
+}
+
+
+
+
+let q4 = prompt(
+  "do you think I like watch the movie ? yes/y no/n "
+).toLowerCase();
+
+switch (q4) {
+  case "yes":
+  case "y":
+    // console.log("great");
+    alert(" who dont like it  ");
+    break;
+
+  case "no":
+  case "n":
+    // console.log("wrong  ");
+    alert(" they are a good movie you miss ");
+
+  default:
+    // console.log(" you must type yes or no ");
+    alert(" you must type yes or no ");
+}
+
+
+
+
+let q5 = prompt("do you think I love some one  ? yes/y no/n  ").toLowerCase();
+
+switch (q5) {
+  case "yes":
+  case "y":
+    // console.log("great");
+    alert(" right when I child hhhhh ");
+    break;
+
+  case "no":
+  case "n":
+    // console.log(" wrong  ");
+    alert(" the love its happend one time your life  ");
+
+    break;
+  default:
+    // console.log(" you must type yes or no ");
+    alert(" you must type yes or no ");
+}
