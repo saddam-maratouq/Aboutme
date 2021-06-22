158c6f8655966b2ad4c3775389212cc2677540a7
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-06-22 23:33:37 +0300

    new feat

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ alert("welcome Dear " + userName);
 
 
 
-let q1 = prompt("do you think Iam like vieo game ? yes/y no/n  ").toLowerCase();
-
+let q1 = prompt('do you think I like vieo game ? yes/y no/n ').toLowerCase();
+console.log( q1);
 switch (q1) {
   case "yes":
 
@@ -87,7 +87,7 @@ switch (q3) {
 
 let q4 = prompt(
   "do you think I like watch the movie ? yes/y no/n "
-).toLowerCase();
+  ).toLowerCase();
 
 switch (q4) {
   case "yes":
@@ -121,10 +121,85 @@ switch (q5) {
   case "no":
   case "n":
     // console.log(" wrong  ");
-    alert(" the love its happend one time your life  ");
+    alert(" the love its happend one time in  your life  ");
 
     break;
   default:
     // console.log(" you must type yes or no ");
     alert(" you must type yes or no ");
 }
+
+
+let q6 = Number(prompt( 'what do you think  the number in my mind ?  '));
+
+for(i = 0 ; i <= 3 ; i++ )
+
+switch(q6){
+
+  case 5 : 
+
+  case 6 : 
+  
+alert(" you are close Dear ")
+
+prompt( 'what do you think  the number in my mind ?  ')
+
+break;
+
+
+case 1 : 
+case 2 :
+case 3 :
+case 4 :
+alert('  you are far gess again plz  ') 
+prompt( 'what do you think  the number in my mind ?  ')
+
+break; 
+
+
+} 
+
+
+
+alert( 'the right answer is 7 ')
+
+
+
+
+ 
+
+let p1= 'cr ronaldo '
+let p2='dimaria'
+let p3= ' cavani '
+let p4= ' messi'
+let p5= ' zeidan '
+let p6=  ' ramos '
+
+let player=['cr ronaldo', 'dimaria', 'cavani','messi ','zeidan','ramos']
+
+let q7=prompt("['cr ronaldo', 'dimaria', 'cavani','messi ','zeidan',ramos ??!]").toLowerCase();
+
+
+
+if (  q7==='cr ronaldo' || q7=== 'zeidan' ) {
+
+  alert(' My fav player  zeidan and cr ronaldo ') 
+
+}else {
+  
+  for(i = 0 ; i <5 ; i++ )
+  prompt( " cr ronaldo / dimaria/ cavani/ messi /zeidan/ramos ") 
+
+  alert(' My fav player  zeidan and cr ronaldo ')  
+
+} 
+
+
+
+alert(' you gess 3  correct answer your scoer its 3/7  ')
+
+
+
+
+
+
---
+++ project.md
@@ -0,0 +1,15 @@
+
+# guessing game  
+
+
+
+***this project someting like a guess game about me,  the user try to guess some fun information about me***
+
+***and I give the user some information in my page like education my age my name etc.....***
+
+
+
+
+
+
+
